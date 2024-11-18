7eaa8ae3c7e62302e5bbda5a357f6eec637c8a58
sist.110 <sist.110@DESKTOP-HK9V3R4>
2024-11-18 14:15:31 +0900

    first commit

---
+++ FirstProject/src/첫번째자바.java
@@ -0,0 +1,92 @@
+/*
+	자바가 호환성이 좋아
+	실무에서 spring을 많이 쓰는데 종류는 java/kotlin(무료)
+	1.대소문자 구분 중요
+	2.들여쓰기 중요=> 가독성 
+	3.명령문 종료시 반드시 ; 사용
+	4.파이썬, kotlin, javascript는 ;를 사용안해
+	5.자바는 { 가 옆에 있어
+	6.문법, 데이터저장->처리->출력
+	  한개 저장 (메모리):변수/상수
+	  여러개 저장 : 배열
+	  			 클래스
+	  단점은 프로그램 종료시 사라져
+	  파일 / 오라클 -> 영구적인 저장방식
+	  
+	  데이터 처리 방식은 연산자 / 제어문이 있어
+	  두개가 합쳐지면 메소드
+
+	  화면출력방식은 3가지
+	  첫번째는 명령프롬프트 : Application ( 여기서 문법을 배운다고 생각 )
+	  두번째는 브라우저출력 : Web Application ( 이게 주력 )
+	  마지막은 모바일출력 : Mobile Application
+	  
+	  
+	  *기본 문법
+	  1. 기능을 모아서 관리 
+	  	단락 : 메소드
+	  	클래스 / 객체 -> 객체지향 프로그램
+	  	프로그램유지(예외처리)  // 여기까지가 수업의 목표(8장)
+	  	
+	  	이쯤에서 1차과제 응용프로그램만들 수 있음(자바로만)
+	  	
+	  	라이브러리 : 자바에서 지원
+	  
+	  
+	  ==========================
+	  
+	  자바끝나면 Web으로 넘어갈 예정
+	  1. HTML : 브라우저에서 실행 ( 자바는 브라우저에서는 일반 텍스트 )
+	   		단점은 투박해 -> css / 자바스크립트
+	  2. JSP ( 요청 -> HTML을 JAVA로 변환해 , 응답 -> JAVA를 HTML로 다시 변환 )
+	  	MVC (모델, 뷰, 컨트롤)
+	  	Front(JS / TS ) / Back(자바/오라클/스프링
+	  	둘 다 하는 걸 Full Stack
+	  	
+	  3. 통합 : Framework : Spriong
+	  			기본 틀 => 메인보드
+	  4. 데이터 분석 : Python / Numpy / Pandas / Matplotlib
+	  
+	  5. NoSq1 => elasticsearch ( 오라클역할 ) 구글이 이걸 활용해서 만들었어
+	  6. 배포 : AWS
+	  약 5.5개월 소요예정
+	  
+	  ===========================
+	  추가
+	  Spring-Boot / JPA / MySQL / React-Query 
+	  Redux / Vue3 / NextJS / TypeScript / NodeJS
+	  	
+	  	
+	  교제순서
+	  자바->오라클->알고르즘(코딩테스트때문에)->HTLM,CSS,JavaScript
+	  ->jsp->Spring-Boot->Python->데이터분석->elasticsearch
+	  
+	  실무에서 많이 사용되는건
+	  Spring, MyBatis,JSP,JavaScript(Jquery)
+	  
+	  
+	  수업과정
+	  첫 주는 개념정리정도?
+	  
+	  월요일-목요일
+	  6교시(15:50) -> 수업
+	  7교시(16:50) -> 문제풀이
+	  8교시(17:50) -> 문제해결 / 하루정리시간
+	  ->프로젝트 진행할 땐 7,8교시 활용
+	  금요일 
+	  1주일동안 배운 내용 정리 / 응용
+	  
+*/	 
+
+public class 첫번째자바 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println("방다혜");
+		{
+			
+		}
+	}
+
+}
