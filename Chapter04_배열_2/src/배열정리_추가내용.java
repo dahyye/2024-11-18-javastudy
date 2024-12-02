49b1780d478b2d563c992cbe38f498cb911c7878
sist.110 <sist.110@DESKTOP-HK9V3R4>
2024-12-02 17:46:43 +0900

    24.12.02

--- Chapter04_배열_2/src/배열정리_추가내용.java
+++ Chapter04_배열_2/src/배열정리_추가내용.java
@@ -11,7 +11,23 @@ public class 배열정리_추가내용 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr= {10, 20, 30, 40, 50};
+		//int[] arr_copy=arr; // 다른 주소값을 가지고 데이터를 변경해도 arr_copy값만 바뀐다
+		int[] arr_copy=arr.clone();
+		System.out.println("arr="+arr);
+		System.out.println("arr_copy="+arr_copy); //arr와 같은 주소값 출력
+		
+		//변경
+		arr_copy[0]=100;
+		arr_copy[1]=200;
+		arr_copy[2]=300;
+		arr_copy[3]=400;
+		arr_copy[4]=500;
 
+		for(int i:arr)
+		{
+			System.out.print(i+" "); //arr의 값도 변경된 걸 확인
+		}
 	}
 
 }
