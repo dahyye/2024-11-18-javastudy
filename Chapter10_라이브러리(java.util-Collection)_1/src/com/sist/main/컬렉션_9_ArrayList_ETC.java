c8af0075ba355b24ce00d730444e3acb5c6e855a
방다혜 <asc8420@naver.com>
2024-12-23 17:47:25 +0900

    24.12.23

--- Chapter10_라이브러리(java.util-Collection)_1/src/com/sist/main/컬렉션_9_ArrayList_ETC.java
+++ Chapter10_라이브러리(java.util-Collection)_1/src/com/sist/main/컬렉션_9_ArrayList_ETC.java
@@ -5,23 +5,49 @@ public class 컬렉션_9_ArrayList_ETC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] names = {"홍길동", "박문수","이순신","강감찬","김두한","홍길동","심청이"};
-		List<String> list = Arrays.asList(names);
-		//배열 ==> List로 변경
-		list.c
-		for(String n : list)
-		{
-			System.out.println(n);
-		}
-		System.out.println();
-		System.out.println("====set중복제거====");
+
+		List<String> list = new ArrayList<String>();
 		
-		Set<String> set = new HashSet<String>();
-		set.addAll(list);
-		for(String n : set)
+		list.add("hello");
+		list.add("java");
+		list.add("List");
+		list.add("hello");
+		list.add("java");
+		
+		
+		Iterator<String> iterator = list.iterator();
+		
+		while(iterator.hasNext())
 		{
-			System.out.println(n);
+			String str = iterator.next();
+			
+			if(str.equals("hello"))
+			{	
+				iterator.remove();
+				continue;
+			}
+			System.out.println(str);
+			
+				
 		}
+		
+		
+		//		String[] names = {"홍길동", "박문수","이순신","강감찬","김두한","홍길동","심청이"};
+//		List<String> list = Arrays.asList(names);
+//		//배열 ==> List로 변경
+//		for(String n : list)
+//		{
+//			System.out.println(n);
+//		}
+//		System.out.println();
+//		System.out.println("====set중복제거====");
+//		
+//		Set<String> set = new HashSet<String>();
+//		set.addAll(list);
+//		for(String n : set)
+//		{
+//			System.out.println(n);
+//		}
 		//set을 사용하면 순서가 다르게 나와
 	}
 
